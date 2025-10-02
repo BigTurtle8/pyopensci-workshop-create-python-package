89b93b16cd9fec368a1ca31819d339b62e05b6f7
Marcus Alagar <69373245+BigTurtle8@users.noreply.github.com>
2025-10-02 18:40:28 +0000

    enh: tests, README

--- tests/unit/test_example.py
+++ tests/unit/test_example.py
@@ -7,7 +7,7 @@ is up to you. This test example provides a single test for the example.py
 module.
 """
 
-from pyospackage_bigturtle8.example import add_numbers
+from pyospackage_bigturtle8.example import *
 
 def test_add_numbers():
     """
@@ -17,4 +17,28 @@ def test_add_numbers():
     """
     out = add_numbers(1, 2)
     expected_out = 3
+    assert  out == expected_out, f"Expected {expected_out} but got {out}"
+
+def test_sub_numbers():
+    """
+    Test that sub_numbers works as expected.
+    """
+    out = sub_numbers(1, 2)
+    expected_out = -1
+    assert  out == expected_out, f"Expected {expected_out} but got {out}"
+
+def test_mul_numbers():
+    """
+    Test that mul_numbers works as expected.
+    """
+    out = mul_numbers(1, 2)
+    expected_out = 2
+    assert  out == expected_out, f"Expected {expected_out} but got {out}"
+
+def test_div_numbers():
+    """
+    Test that div_numbers works as expected.
+    """
+    out = div_numbers(1, 2)
+    expected_out = 0.5
     assert  out == expected_out, f"Expected {expected_out} but got {out}"
